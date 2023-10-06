daff7700cd2e7bd77b72b24861e18d474cc56422
Finatic-pranav <prsh4120.ps@gmail.com>
2023-10-06 18:24:08 +0530

    live site and code enabling

--- src/components/Works/Work.js
+++ src/components/Works/Work.js
@@ -9,6 +9,8 @@ const skills = [
     key: 1,
     img: portfolio1,
     title: "Task Management App",
+    liveSite:'/',
+    code:'/',
     content:
       "Create a task management application where users can create, update, and delete tasks. You can implement features like task categorization, due dates, priority levels, and the ability to mark tasks as completed. Use Redux to manage the state of tasks and their various attributes",
   },
@@ -16,6 +18,8 @@ const skills = [
     key: 2,
     img: portfolio2,
     title: "E-commerce Platform",
+    liveSite:'https://ecom-finatics-projects.vercel.app',
+    code:'https://github.com/Finatic-pranav/ecom-frontend',
     content:
       "Build an e-commerce platform with features like product listing, search, product details, shopping cart, and user authentication. Use Redux to manage the shopping cart state and user authentication status across different components",
   },
@@ -23,6 +27,8 @@ const skills = [
     key: 3,
     img: portfolio3,
     title: "Note taking App",
+    liveSite:'/',
+    code:'/',
     content:
       "Create a note-taking application where users can create, edit, and delete notes. You can enhance the app with features like rich text formatting, categorization, and synchronization across devices. Redux can be used to manage the state of notes and their metadata",
   },
@@ -40,14 +46,22 @@ const Work = () => {
         boundaries of modern web development.
       </span>
       <div className="worksImgs">
-        {skills.map((skill) => {
+        {skills.map((skill,index) => {
           return (
-            <div className="worksDetails">
+            <div key={index} className="worksDetails">
+              <h3>{skill.title}</h3>
               <span className="hovermee">Hover Me!</span>
               <img src={skill.img} alt={skill.title} className="worksImg" />
               <div className="worksDetail">
-                <h2>{skill.title}</h2>
                 <p>{skill.content}</p>
+                <div>
+                  <button className="worksDetailBtn">
+                    <a href={skill.liveSite} style={{textDecoration:'none'}}>Live site</a>
+                  </button>
+                  <button className="worksDetailBtn">
+                    <a href={skill.code} style={{textDecoration:'none'}}>code</a>
+                  </button>
+                </div>
               </div>
             </div>
           );
